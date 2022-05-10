566676ebd85a26c1d7935d6f4766566358fed395
sandiemann <sanchit.bhavsar@gmail.com>
2022-05-10 18:21:39 +0200

    added setup file

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+from io import open
+from os import path
+
+import pathlib
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# automatically captured required modules for install_requires in requirements.txt
+with open(path.join(HERE, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if ('git+' not in x) and (
+    not x.startswith('#')) and (not x.startswith('-'))]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs \
+                    if 'git+' not in x]
+setup (
+ name = 'clarc',
+ description = 'A simple command-line tool to archive data',
+ version = '1.0.0',
+ packages = find_packages(), # list of all packages
+ install_requires = install_requires,
+ python_requires='>=2.7', # any python greater than 2.7
+ entry_points='''
+        [console_scripts]
+        clarc=clarc.__main__:main
+    ''',
+ author="Sanchit Bhavsar",
+ keyword="commands, cli, archive, db, sqlite",
+ long_description=README,
+ long_description_content_type="text/markdown",
+ license='MIT',
+ url='https://github.com/sandiemann/clarc-cli',
+ download_url='https://github.com/sandiemann/clarc-cli/archive/1.0.0.tar.gz',
+  dependency_links=dependency_links,
+  author_email='sanchit.bhavsar@gmail.com',
+  classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ]
+)
